18eccc7293cdc920226c4b852d8a4b939ad9a670
Gustavo Costa <gustavocostaalmeida@gmail.com>
2021-05-19 18:11:56 -0400

    first commit

---
+++ CA.Infra.Data/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CA.Infra.Data.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
